b1602f6d9b9b791df575f814062919049b625320
Artem Yuzhakov <artem.yuzhakov@stroeer.de>
2021-10-07 13:47:24 +0200

    migrate to embedding 2

--- android/src/main/kotlin/de/medienkontorfulda/sourcepoint_cmp/SourcepointCmpPlugin.kt
+++ android/src/main/kotlin/de/medienkontorfulda/sourcepoint_cmp/SourcepointCmpPlugin.kt
@@ -3,9 +3,15 @@ package de.medienkontorfulda.sourcepoint_cmp
 import android.app.Activity
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import androidx.appcompat.app.AppCompatActivity
 
 /** SourcepointCmpPlugin */
-class SourcepointCmpPlugin {
+class SourcepointCmpPlugin : FlutterPlugin, ActivityAware  {
+
+ /
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
@@ -15,10 +21,36 @@ class SourcepointCmpPlugin {
         // We stop the registration process as this plugin is foreground only.
         return;
       }
-      val interstitialChannel =
-              MethodChannel(registrar.messenger(), "sourcepoint_cmp")
+      val interstitialChannel = MethodChannel(registrar.messenger(), "sourcepoint_cmp")
       interstitialChannel.setMethodCallHandler(SourcepointCmp(registrar, interstitialChannel, activity))
 
     }
   }
+
+  // region FlutterPlugin
+  final override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+  }
+
+  final override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+  }
+  // endregion
+
+  // region ActivityAware
+  final override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity as AppCompatActivity
+
+    val interstitialChannel = MethodChannel(registrar.messenger(), "sourcepoint_cmp")
+    interstitialChannel.setMethodCallHandler(SourcepointCmp(registrar, interstitialChannel, activity))
+
+  }
+
+  final override fun onDetachedFromActivity() {
+  }
+
+  final override fun onDetachedFromActivityForConfigChanges() =
+    onDetachedFromActivity()
+
+  final override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) =
+    onAttachedToActivity(binding)
+  // endregion
 }
